a70f7756b4c8c9298be6f45cd7ef694692a12b5c
Robert König <robsoft.softwaredevelopment@gmail.com>
2021-06-01 20:19:14 +0200

    details.html added

--- js/readJSON.js
+++ js/readJSON.js
@@ -1,17 +1,35 @@
 
 function zeigeDaten(daten) {
 
+	
+/*
     //Rückgabe der d3.selectAll - Methode in variable p speichern.(Alle Kindelemente von content, die p- Elemente sind.) Am Anfang gibt es noch keine.
-    let p = d3.select("#content").selectAll("p").data(daten);
+    let p = d3.select("#demo").selectAll("p").data(daten);
 
     //.enter().append(): Daten hinzufuegen falls es mehr Daten als Elemente im HTML gibt.
     //geschieht hier für jede Zeile von daten.
     p.enter().append("p")
         .text(function (daten) {
-            return "Uhrzeit: " + daten.datum + " Wert:" + JSON.stringify(daten.werte, null, 2);
+            // JSON als Variable speichern
+            const myJSON = JSON.stringify(daten.werte, null, 10);
+//            var res = myJSON.split(",");
+//            console.log(res[0]);
+    
+            return daten;
         });
+
     //.exit().remove(): Daten löschen, falls es mehr Elemente im HTML als Daten gibt.
     p.exit().remove();
+
+*/
+
+    // Version 2, set single JSON rein 
+    const myJSON = JSON.stringify(daten.werte, null, "");
+    var firstEntry = daten[0];
+
+    const json = JSON.stringify(JSON.stringify(firstEntry.werte, null, ""));
+    let t = d3.select("#singleJSON").text(json);
+
 }
 
 function empfangeDaten(datenEmpfangen,error) {
